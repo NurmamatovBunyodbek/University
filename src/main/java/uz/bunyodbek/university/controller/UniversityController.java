adf9910050a038f3aa982987f1fef96c472ea4ca
Bunyodbek Nurmamatov <bunyodbeknurmamatov@gmail.com>
2024-11-08 18:59:14 +0500

    add univer

--- src/main/java/uz/bunyodbek/university/controller/UniversityController.java
+++ src/main/java/uz/bunyodbek/university/controller/UniversityController.java
@@ -1,4 +1,27 @@
 package uz.bunyodbek.university.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import uz.bunyodbek.university.model.Result;
+import uz.bunyodbek.university.model.University;
+import uz.bunyodbek.university.service.UniversityService;
+
+@RestController
 public class UniversityController {
+
+
+    @Autowired
+    UniversityService universityService;
+
+
+    @PostMapping("/user")
+    public Result add(@RequestBody University university){
+        Result univer = universityService.createUniver(university);
+        return univer;
+    }
+
+
+
 }
--- src/main/java/uz/bunyodbek/university/service/UniversityService.java
+++ src/main/java/uz/bunyodbek/university/service/UniversityService.java
@@ -1,4 +1,29 @@
 package uz.bunyodbek.university.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import uz.bunyodbek.university.model.Result;
+import uz.bunyodbek.university.model.University;
+import uz.bunyodbek.university.repository.UniversityRepo;
+@Service
 public class UniversityService {
+
+
+    @Autowired
+    UniversityRepo universityRepo;
+
+
+    public Result createUniver(University university){
+        University university1 = new University();
+
+        university1.setName(university.getName());
+        university1.setAddress(university.getAddress());
+        universityRepo.save(university1);
+
+        return new Result(true,"Create");
+    }
+
+
+
+
 }
